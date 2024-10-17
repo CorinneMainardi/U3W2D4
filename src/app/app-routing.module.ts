75f6aa863ac50d3fb63117abfa2d9319f0ffd14e
CorinneMainardi <147351705+CorinneMainardi@users.noreply.github.com>
2024-10-17 17:50:51 +0200

    correzione errori

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { FavoritesComponent } from './pages/favorites/favorites.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {
-    path: '/favorites',
+    path: 'favorites',
     component: FavoritesComponent,
   },
 ];
